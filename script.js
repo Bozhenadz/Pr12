ba2dd7a7ac91eb2d66b281ae163cdedf3cbd4bd5
Bozhenadz <159878194+Bozhenadz@users.noreply.github.com>
2024-05-13 16:43:34 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+document.getElementById('submitButton').addEventListener('click', function() {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var meal = document.querySelector('input[name="meal"]:checked');
+    var foods = document.querySelectorAll('input[name="food"]:checked');
+    var comment = document.getElementById('comment').value;
+    var resultDiv = document.getElementById('result');
 
+    if (!username || !email || !meal || foods.length === 0 || !comment) {
+        resultDiv.textContent = "Будь ласка, заповніть всі поля!";
+        return;
+    }
+
+    var mealValue = meal.value;
+    var foodsArray = [];
+    foods.forEach(function(food) {
+        foodsArray.push(food.value);
+    });
+    var foodsString = foodsArray.join(', ');
+
+    var resultText = username + ' (' + email + ') з’їв на ' + mealValue + ' ' + foodsString + ' та залишив такий коментар: "' + comment + '"';
+    resultDiv.textContent = resultText;
+});
